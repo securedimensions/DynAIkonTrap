de88049e1f9b89bf438910f0ca7c4ace638a2e48
Miklas Riechmann <mhr@dynaikon.com>
2020-11-24 16:57:21 +0000

    Add advice for updating RPi - Error out if installing dependencies failed

--- setup.sh
+++ setup.sh
@@ -23,12 +23,20 @@ done
 if [ $python_command == 0 ]
 then
     echo "Need python >= 3.7; install with:"
-    echo "  apt install python3.7"
+    echo "  sudo apt install python3.7"
     exit -1
 fi
 
 ## Install dependencies
 sudo -p "[sudo] password to install dependencies> " apt install -y libaom0 libatlas3-base libavcodec58 libavformat58 libavutil56 libbluray2 libcairo2 libchromaprint1 libcodec2-0.8.1 libcroco3 libdatrie1 libdrm2 libfontconfig1 libgdk-pixbuf2.0-0 libgfortran5 libgme0 libgraphite2-3 libgsm1 libharfbuzz0b libilmbase23 libjbig0 libmp3lame0 libmpg123-0 libogg0 libopenexr23 libopenjp2-7 libopenmpt0 libopus0 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpixman-1-0 librsvg2-2 libshine3 libsnappy1v5 libsoxr0 libspeex1 libssh-gcrypt-4 libswresample3 libswscale5 libthai0 libtheora0 libtiff5 libtwolame0 libva-drm2 libva-x11-2 libva2 libvdpau1 libvorbis0a libvorbisenc2 libvorbisfile3 libvpx5 libwavpack1 libwebp6 libwebpmux3 libx264-155 libx265-165 libxcb-render0 libxcb-shm0 libxfixes3 libxrender1 libxvidcore4 libzvbi0
+
+if [ $? -ne 0 ]
+then
+    echo "There was an error installing dependencies (see above)"
+    echo "Are you sure your device is up-to-date, update with:"
+    echo "  sudo apt update && sudo apt upgrade"
+    exit -1
+fi
 
 ## Ensure virtual environment package is installed
 dpkg -s python3-venv > /dev/null 2>&1
