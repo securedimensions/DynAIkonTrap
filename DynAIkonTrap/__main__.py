0fa4e73996fe8b1a4735c1667bffdd3f3b98b230
Miklas Riechmann <mhr@dynaikon.com>
2021-08-08 13:36:42 +0000

    Update sensor parsing
    We now use the updated serial interface to the urSense board
    
    Also minor changes to SensorLog definition

--- DynAIkonTrap/__main__.py
+++ DynAIkonTrap/__main__.py
@@ -33,11 +33,11 @@ argparse.add_argument(
 )
 args = argparse.parse_args()
 
-from DynAIkonTrap.camera import Camera  # , MockCamera
+from DynAIkonTrap.camera import Camera
 from DynAIkonTrap.filtering import Filter
-from DynAIkonTrap.comms import Sender, Writer
+from DynAIkonTrap.comms import Output
 from DynAIkonTrap.sensor import SensorLogs
-from DynAIkonTrap.settings import load_settings, OutputMode
+from DynAIkonTrap.settings import load_settings
 
 # Make Ctrl-C quit gracefully
 def handler(signal_num, stack_frame):
@@ -65,11 +65,7 @@ getLogger().setLevel(settings.logging.level)
 camera = Camera(settings=settings.camera)
 filters = Filter(read_from=camera, settings=settings.filter)
 sensor_logs = SensorLogs(settings=settings.sensor)
-
-if settings.output.output_mode == OutputMode.SEND:
-    Sender(settings=settings.output, read_from=(filters, sensor_logs))
-else:
-    Writer(settings=settings.output, read_from=(filters, sensor_logs))
+Output(settings=settings.output, read_from=(filters, sensor_logs))
 
 while True:
     pass
