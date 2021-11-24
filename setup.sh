66a12b9b044d9acabd9d324b2dff845d9517eb08
rossg <ross.gardiner@dynaikon.com>
2021-11-24 20:15:42 +0000

    syntax fix

--- setup.sh
+++ setup.sh
@@ -92,11 +92,10 @@ fi
     ## Install platform dependant tflite_runtime
     if cat /proc/device-tree/model | grep -q 'Raspberry Pi 4'; then
         pip install python_wheels/tflite_runtime/rpi_4/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
-    fi
     elif cat /proc/device-tree/model | grep -q 'Raspberry Pi Zero'; then
         pip install python_wheels/tflite_runtime/rpi_0/tflite_runtime-2.7.0-cp37-cp37m-linux_armv6l.whl
     else 
-        ## This is a different device to downloaded wheels, try reccomended tflite_runtime install, connects to web
+        ## This is a different device to downloaded wheels, try default tflite_runtime install
         pip install --extra-index-url https://google-coral.github.io/py-repo/ tflite_runtime
     fi
 ## Create a "launcher" script that can also be called via `nohup`
